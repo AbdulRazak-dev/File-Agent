41c23a68c7fbd64645f2196ee907f87c9c61ebb7
Abd_rzk_ <abdulrazak4250@gmail.com>
2026-09-11 15:20:53 +0530

    Create document_processor.py

---
+++ app/document_processor.py
@@ -0,0 +1,18 @@
+from pypdf import PdfReader
+
+
+def extract_pdf_text(file_path):
+    reader = PdfReader(file_path)
+
+    pages = []
+
+    for page_number, page in enumerate(reader.pages, start=1):
+
+        text = page.extract_text() or ""
+
+        pages.append({
+            "page": page_number,
+            "text": text
+        })
+
+    return pages
